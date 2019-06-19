c8370ad98f8a657c80bdd00856c733c72ff3116e
bowen <bowen.nan@icloud.com>
2019-06-19 12:29:46 +0800

    update

--- build-gitlab.sh
+++ build-gitlab.sh
@@ -4,5 +4,5 @@ docker stop pr.lgl
 
 docker rm pr.lgl
 
-docker run -d --name=pr.lgl --publish 8443:443 --publish 8899:80 --publish 2222:22 --restart always --volume /home/bowen/pr/data/gitlab/config:/etc/gitlab --volume /home/bowen/pr/data/gitlab/logs:/var/log/gitlab --volume /home/bowen/pr/data/gitlab/data:/var/opt/gitlab --volume /home/bowen/pr/natapp:/natapp gitlab/gitlab-ce
+docker run -d --name=pr.lgl --publish 8443:443 --publish 8899:80 --publish 2222:22 --restart always --volume /usr/local/gitlab/config:/etc/gitlab --volume /usr/local/gitlab/logs:/var/log/gitlab --volume /usr/gitlab/data:/var/opt/gitlab gitlab/gitlab-ce
 
