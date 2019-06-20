688db58d76696f3a1cb4431262646e8f6470a304
Bowen <bowen.nan@icloud.com>
2019-06-20 17:20:09 +0800

    d

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM 1stclass/docker-python3-django2-alpine-base
 MAINTAINER bowen
 RUN ["mkdir","-p","/usr/local/pr/web"]
 COPY ./web /usr/local/pr/web
+RUN ["pip","install","pymongo"]
--- natapp/grepport.sh
+++ natapp/grepport.sh
@@ -1,11 +1,14 @@
 while [ "1" = "1" ]
 do
-#grep port
-cat /natapp.log|grep -o cc:[0-9]*|sed 's/cc:/g' > port
-cat port
-# update to web
-var=$(cat /port)
-echo extnal_port is ${var}
-rm /natapp.log
-sleep 10s
+	if [ ! -f "natapp.log" ];then
+		#grep port
+		cat natapp.log|grep -o cc:[0-9]*|grep -o [0-9]* > port
+		cat port
+		# update to web
+		var=$(cat port)
+		time=$(date "+%Y-%m-%d %H:%M:%S")
+		echo extnal_port is $var
+		rm natapp.log
+	fi
+	sleep 10s
 done
